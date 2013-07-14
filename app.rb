bab76e1b3d63fa3c16b8c0961fa3fc35301c9991
Sean Moon <ssamoon@ucla.edu>
2013-07-14 16:49:39 +0900

    Move default

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require 'cuba'
 
 Cuba.define do
-  on default do
-    res.status = 404
-    res.write "404 Not Found"
-  end
-
   on get do
     on root do
       res.status = 200
       res.write "Synthdex"
+    end
+
+    on default do
+      res.status = 404
+      res.write "404 Not Found"
     end
   end
 end
